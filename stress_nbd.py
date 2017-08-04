0144c9226a385fa3b9368d6f9c46388d979373f1
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2017-08-04 17:39:02 +0100

    General Improvements
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- stress_nbd.py
+++ stress_nbd.py
@@ -8,10 +8,11 @@ import csv
 
 parser = argparse.ArgumentParser(description='Process nbd server options/')
 parser.add_argument('-s', '--server', help='nbd-server address')
-parser.add_argument('-b', '--blockdev', help='block device you wish to use .e.g. /dev/nbd2')
+parser.add_argument('-b', '--blockdev', default='/dev/nbd2', help='block device you wish to use .e.g. /dev/nbd2')
 parser.add_argument('-n', '--nbdtype', help='type of nbd-server e.g. nbd-tool.2.1.3')
 parser.add_argument('-t', '--test', help='type of type you would like to run')
 parser.add_argument('--setup', dest='setup', default=False, help="Set if throughput tests need to be setup")
+parser.add_argument('-p', '--port', default='')
 
 args = parser.parse_args()
 
@@ -26,8 +27,10 @@ def server_connect(ip, server, blockdev, name):
     	modprobe_command = "modprobe nbd"
     	ssh_host(ip, modprobe_command)
     	read_command = "nbd-client -N %s %s %s" %(name, server, blockdev)
+    	print read_command
     	startTime = datetime.now()
     	connection = ssh_host(ip, read_command)
+    	print "Connected"
     	write_to_csv("multi.csv", [ip, blockdev])
     	print "Connected to server from %s %s" %(ip, blockdev)
     except:
@@ -37,8 +40,9 @@ def server_read(ip, blockdev, num=2):
 	try:
 		startTime = datetime.now()
 		print "Reading blocks on %s %s" %(ip, blockdev)
-		check_blocks = "dd if=%s of=./output%s" %(blockdev, num)
+		check_blocks = "dd if=%s of=./output%s bs=64K" %(blockdev, num)
 		blocks = ssh_host(ip, check_blocks)
+		ssh_host(ip, "sync")
 		read_time = [datetime.now() - startTime, datetime.now(), ip, blockdev]
 		write_to_csv("time.csv", read_time)
 		server_disconnect(ip, blockdev)
@@ -83,7 +87,7 @@ def setup_throughput_tests(ip):
 
 def throughput_test(ip, server, blockdev, name):
 	if name == "nbd-server":
-		port = "2000"
+		port = "10809"
 	else:
 		port = ""
 	test_command = "cd /root/nbd-3.15.2/tests/run/; ./nbd-tester-client %s %s -N %s" %(server, port, blockdev)
@@ -94,9 +98,10 @@ def throughput_test(ip, server, blockdev, name):
 	print "------------------------------------------------------------"
 	scp_command = "sudo sshpass -p xenroot scp root@%s:/root/log-%s.txt ~/log.txt" %(ip, ip)
 
-def repeated_connect(ip, server, blockdev, name):
+def repeated_connect(ip, server, blockdev, name, port, nbd_type):
     x = 0 
-    while x < 1:
+    server = "%s %s" %(server, port)
+    while x < 100:
     #while True:
     	startTime = datetime.now()
         try:
@@ -106,12 +111,12 @@ def repeated_connect(ip, server, blockdev, name):
             print "Connection was refused after %s connections" %x
             break
         x += 1
-        print "It took %s seconds to read data" %(datetime.now() - startTime)
+        print "Connection %s: It took %s seconds to connect and read data" %(x, datetime.now() - startTime)
         
-        read_time = read_time + [datetime.now() - startTime, datetime.now(), x]
+        read_time = read_time + [datetime.now() - startTime, x, nbd_type]
         write_to_csv("report.csv", read_time)
         if x > 0:
-        	sleep(10)
+        	sleep(5)
 
 def simultaneous_connect(ip, server, blockdev, name):
     threads=[]
@@ -130,11 +135,13 @@ if __name__ == '__main__':
 	password = 'xenroot'
 	username = 'root'
 	server = args.server
+	port = args.port
 	blockdev = args.blockdev
 	nbd_type = args.nbdtype
 	test_type = args.test
 	#These the the VMs on the same host as the dt56 server
 	local_ips = ["10.62.114.2", "10.62.114.1"]
+	dt56_ips = ["10.71.77.216", "10.71.77.89"]
 
 	ips = ["10.62.114.2", "10.62.114.3", "10.62.114.1", "10.71.77.216", "10.71.77.89", "10.62.114.9", "10.62.114.11", "10.62.114.12", "10.62.114.13", "10.62.114.14", "10.62.114.17", "10.62.114.16", "10.62.114.18", "10.62.114.19", "10.62.114.21", "10.62.114.22", "10.62.114.24", "10.62.114.23", "10.62.114.25", "10.62.114.26"]
 
@@ -147,12 +154,12 @@ if __name__ == '__main__':
 	#Commands currently failing - INVESTIGATE
 	#subprocess.check_output(['rm multi.csv time.csv stats.csv'])
 	#subprocess.check_output(['touch multi.csv time.csv stats.csv'])
-    multi = ["IP", "Blockdev"]
-    write_to_csv("multi.csv", multi)
-    stats = ["CPU%", "Memory (KB)", "Open FDs", "Time"]
-    write_to_csv("stats.csv", stats)
-    time = ["Connection Time", "Time", "IP", "Blockdev"]
-    write_to_csv("time.csv", time)
+	multi = ["IP", "Blockdev"]
+	write_to_csv("multi.csv", multi)
+	stats = ["CPU%", "Memory (KB)", "Open FDs", "Time"]
+	write_to_csv("stats.csv", stats)
+	time = ["Connection Time", "Time", "IP", "Blockdev"]
+	write_to_csv("time.csv", time)
 
 	if test_type == "setup":
 		for ip in ips:
@@ -166,10 +173,10 @@ if __name__ == '__main__':
 	if test_type == "repeat":
 		log_file = "repeat-%s" %datetime.now()
 		#make_log(log_file)
-		report = ["Read time", "Time", "IP", "Blockdev", "Total connection time", "Connection no."]
+		report = ["Read time", "Time", "IP", "Blockdev", "Total connection time", "Connection no.", "Type"]
 		write_to_csv("report.csv", report)
 		for ip in local_ips:
-			t = threading.Thread(target=repeated_connect, args=(ip, server, blockdev, name,))
+			t = threading.Thread(target=repeated_connect, args=(ip, server, blockdev, name, port, nbd_type))
 			threads.append(t)
 			t.start()
 		while t.is_alive():
@@ -177,11 +184,18 @@ if __name__ == '__main__':
 			sleep(1) 
 
 	if test_type == "sim":
-		for ip in ips:
-			ip_threads = simultaneous_connect(ip, server, "/dev/nbd", name)
-			threads = threads + ip_threads
-		for t in threads:
-			t.start()
-		while t.is_alive():
-			host_stats(server, nbd_type)
-			sleep(1) 
+		for a in xrange(1, len(ips)+1):
+			print "Trying read with %s simultaneous connections" %a
+			write_to_csv("time.csv", ["%s connections"])
+			threads = [] 
+			for b in range(0, a):
+				ip_threads = simultaneous_connect(ips[b], server, "/dev/nbd", name)
+				threads = threads + ip_threads
+			for t in threads:
+				t.start()
+
+			while t.is_alive():
+				host_stats(server, nbd_type)
+				sleep(1)
+			#Adding in sleep to let it settle down		
+			sleep(5)
